6e3211d6c491ab56a8ac16a26d9e097fb5f145aa
beatrictrac <127038715+beatrictrac@users.noreply.github.com>
2023-04-06 18:28:02 +0200

    Delete Entities directory

--- Entities/Character.cpp
+++
@@ -1,6 +0,0 @@
-/*----------------------------------------------------------------------------------
-Implementazione della superclasse Character ("Personaggio")
-----------------------------------------------------------------------------------*/
-
-#include <ncurses/ncurses.h>
-#include "Character.h"
