ed8f3852acbf36d6009ac277f59288ed34ace5ac
LittleMugGames <li.xu@studio.unibo.it>
2023-09-05 21:39:24 +0200

    spawn

--- SetEnemiesList.h
+++ SetEnemiesList.h
@@ -7,7 +7,7 @@ I nemici vengono gestiti per mezzo di una lista dinamica.
 #include <algorithm>    // std::random_shuffle
 
 
-const int NUM_ENEMIES_DEFAULT = 15; //numero di nemici presenti di default sulla mappa
+const int NUM_ENEMIES_DEFAULT = 3; //numero di nemici presenti di default sulla mappa
 const int NUM_ENEMIES_GROWTH = 5; //numero di livelli di difficoltà necessari per aumentare il numero di nemici di 1
 
 struct nodo{    
