87ad7718b90966de006a33f8aa008c5f59aa1de5
beatrictrac <beatrice.ravaglia3@studio.unibo.it>
2023-09-06 18:52:50 +0200

    ultima mini modifica

--- SetEnemiesList.h
+++ SetEnemiesList.h
@@ -7,8 +7,8 @@ I nemici vengono gestiti per mezzo di una lista dinamica.
 #include <algorithm>    // std::random_shuffle
 
 
-const int NUM_ENEMIES_DEFAULT = 2; //numero di nemici presenti di default sulla mappa
-const int NUM_ENEMIES_GROWTH = 3; //numero di livelli di difficoltà necessari per aumentare il numero di nemici di 1
+const int NUM_ENEMIES_DEFAULT = 3; //numero di nemici presenti di default sulla mappa
+const int NUM_ENEMIES_GROWTH = 2; //numero di livelli di difficoltà necessari per aumentare il numero di nemici di 1
 
 struct nodo{    
     Enemy *e;
